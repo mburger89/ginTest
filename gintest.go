901350a532cdbb627d83fa6401605031815769b8
max burger <mburger89@me.com>
2024-03-25 22:31:31 -0700

    adds hello route

--- gintest.go
+++ gintest.go
@@ -12,5 +12,10 @@ func main() {
       "message": "pong",
     })
   })
+  r.GET("/hello", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+      "message": "world",
+    })
+  })
   r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
